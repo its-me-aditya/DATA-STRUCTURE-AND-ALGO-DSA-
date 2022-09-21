ff941762f3cf5a79b7ca1ed0d34a9706e261a2fa
Aditya singh <107112101+its-me-aditya@users.noreply.github.com>
2022-09-21 17:10:56 +0530

    Create Assigment-1.cpp

---
+++ Assigment-1.cpp
@@ -0,0 +1,27 @@
+Question-1
+#include <iostream>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
+----------------------------------------------------------------------------------------------------
+Question-2
+include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+     
+     int a; int b; int c;
+     cin>>a>>b>>c;
+     int sum =a+b+c;
+     cout<<sum<<endl;
+    return 0;
+}
+-----------------------------------------------------------------------------------------------------
