b47373e8ca6f8de12a82ad5ccab9278e13d3fd45
Aditya singh <107112101+its-me-aditya@users.noreply.github.com>
2023-02-13 23:22:06 +0530

    Tree:inorder_traversal.cpp

--- Assignment_10.cpp/Tree:inorder_traversal.cpp
+++ Assignment_10.cpp/Tree:inorder_traversal.cpp
@@ -1,26 +1,30 @@
-/* you only have to complete the function given below.  
-Node is defined as  
-class Node {
-    public:
-        int data;
-        Node *left;
-        Node *right;
-        Node(int d) {
-            data = d;
-            left = NULL;
-            right = NULL;
-        }
-};
-*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
 
-    void inOrder(Node *root) {
-        if(root == NULL)
-        {
-            return;
-        }
-        inOrder(root -> left);
-        cout << root -> data << " ";
-        inOrder(root -> right);
-            
+void reInorder(TreeNode *root,vector <int>&ans){
+    if(!root){
+        return;
     }
-Footer
+    reInorder(root -> left,ans);
+    ans.push_back(root->val);
+    reInorder(root ->right,ans);
+}
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector <int> ans ;
+        reInorder(root,ans);
+        return ans;
+
+    }
+
+};
